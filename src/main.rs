b0c358a34d05ef2349d46ccad15d0d601d5a62c8
parkyes90 <parkyes90@gmail.com>
2021-01-17 19:35:50 +0900

    ownership #1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
 fn main() {
-    println!("숫자 맞히기");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("입력한 값을 읽지 못했습니다.");
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+    println!("{}", s1);
+    println!("{}", s3);
+}
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("입력한 값: {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("입력한 숫자가 작습니다."),
-            Ordering::Greater => println!("입력한 숫자가 큽니다."),
-            Ordering::Equal => {
-                println!("정답!");
-                break;
-            }
-        }
-    }
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
