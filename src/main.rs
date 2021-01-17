6509fb4ba3cbfef070932227430c86149a2ff5e6
parkyes90 <parkyes90@gmail.com>
2021-01-17 19:49:16 +0900

    ownership #3 with slice

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("{}의 길이는 {}입니다.", s1, len)
+    let s1 = String::from("             hello");
+    let len = first_word(&s1);
+    println!("{}의 첫 단어는 {}입니다.", s1, len)
 }
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
