dc6ebc205752dc780bbd625f0d0a0734ab18a0ce
parkyes90 <parkyes90@gmail.com>
2021-01-17 19:38:06 +0900

    ownership #2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 fn main() {
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-    println!("{}", s1);
-    println!("{}", s3);
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("{}의 길이는 {}입니다.", s1, len)
 }
-
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
