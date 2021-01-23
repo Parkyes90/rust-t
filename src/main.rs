e9a4bca334dade27caaa49807f5896b4480c55ab
parkyes90 <parkyes90@gmail.com>
2021-01-23 22:40:39 +0900

    struct #2

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let user1 = User {
-        email: String::from("parkyes90@gmail.com"),
-        username: String::from("parkyes90"),
-        active: true,
-        sign_in_count: 1,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 30,
     };
-    let s1 = String::from("             hello");
-    let len = first_word(&s1);
-    println!("{}의 첫 단어는 {}입니다.", s1, len);
-    println!(
-        "{},{},{},{}",
-        user1.email, user1.username, user1.active, user1.sign_in_count
-    );
+    println!("사각형의 면적: {} 제곱 픽셀", area(&rect1));
+    println!("rect1: {:?}", rect1);
 }
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    &s[..]
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
