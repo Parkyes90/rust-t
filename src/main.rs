a385c666a43cf8251cc44e712a19f4693e601288
parkyes90 <parkyes90@gmail.com>
2021-01-23 22:48:30 +0900

    struct #3

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,35 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 30,
     };
-    println!("사각형의 면적: {} 제곱 픽셀", area(&rect1));
-    println!("rect1: {:?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 40,
+        height: 40,
+    };
+    let square = Rectangle::square(32);
+    println!("rect1: {:?}", rect1.area());
+    println!("rect2은 rect1을 포함하는가??: {}", rect2.can_hold(&rect1));
+    println!("square {:?}", square);
 }
