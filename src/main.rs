48418034e0f50b6661562238824bca5c7131b111
parkyes90 <parkyes90@gmail.com>
2021-01-23 22:31:00 +0900

    struct #1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
+    let user1 = User {
+        email: String::from("parkyes90@gmail.com"),
+        username: String::from("parkyes90"),
+        active: true,
+        sign_in_count: 1,
+    };
     let s1 = String::from("             hello");
     let len = first_word(&s1);
-    println!("{}의 첫 단어는 {}입니다.", s1, len)
+    println!("{}의 첫 단어는 {}입니다.", s1, len);
+    println!(
+        "{},{},{},{}",
+        user1.email, user1.username, user1.active, user1.sign_in_count
+    );
 }
 fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
